c115c6cc84b21dc3df83d4b280ca9ea85c1bd741
Yasin <yasinhassan004@gamil.com>
2026-07-10 12:11:39 +0300

    Add CASE expression to classify attendees by AGE

--- queries/08_case.sql
+++ queries/08_case.sql
@@ -10,3 +10,15 @@ SELECT
         ELSE 'VIP'
     END AS price_category
 FROM tickets;
+
+SELECT
+    first_name,
+    last_name,
+    age,
+    CASE
+        WHEN age < 25 THEN 'Youth'
+        WHEN age BETWEEN 25 AND 40 THEN 'Adult'
+        ELSE 'Senior'
+    END AS age_group
+FROM attendees;
+
