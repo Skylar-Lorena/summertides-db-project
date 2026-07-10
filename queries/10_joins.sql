e9135e5273bb6fb7cf2d44036a56014490eb6cee
Ccode-400 <stanleymngige@gmail.com>
2026-07-10 12:03:15 +0300

    List every performance with both the artist name and stage name

--- queries/10_joins.sql
+++ queries/10_joins.sql
@@ -6,3 +6,8 @@ SELECT ar.artist_name, s.stage_name
 FROM artists ar
 JOIN performances p ON ar.artist_id = p.artist_id
 JOIN stages s ON p.stage_id = s.stage_id;
+
+SELECT p.performance_name, ar.artist_name, s.stage_name
+FROM performances p
+JOIN artists ar ON p.artist_id = ar.artist_id
+JOIN stages s ON p.stage_id = s.stage_id;
