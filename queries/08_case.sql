f255ebfefb7ec2483ddc1acecd114ddad4424716
Ccode-400 <stanleymngige@gmail.com>
2026-07-10 11:19:28 +0300

    added attendees classification

--- queries/08_case.sql
+++ queries/08_case.sql
@@ -5,3 +5,11 @@ SELECT ticket_type, price,
            ELSE 'VIP'
        END AS price_category
 FROM tickets;
+
+SELECT full_name, age,
+       CASE
+           WHEN age < 25 THEN 'Youth'
+           WHEN age < 60 THEN 'Adult'
+           ELSE 'Senior'
+       END AS age_category
+FROM attendees;
