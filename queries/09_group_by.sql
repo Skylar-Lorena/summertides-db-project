95e26c1494e776c087c6d4dca6773d556ee57b24
Ccode-400 <stanleymngige@gmail.com>
2026-07-10 11:40:06 +0300

    Count attendees by city

--- queries/09_group_by.sql
+++ queries/09_group_by.sql
@@ -3,3 +3,5 @@ SELECT COUNT(*) AS total_attendees FROM attendees;
 SELECT AVG(price) AS average_ticket_price FROM tickets;
 
 SELECT MIN(price) AS min_price, MAX(price) AS max_price FROM tickets;
+
+SELECT city, COUNT(*) AS attendee_count FROM attendees GROUP BY city ORDER BY attendee_count DESC;
