aa4fe2db726b8bdac587c552fdf9f0d136f8a291
Jairus <bg.jairtom098@gmai.com>
2026-07-10 12:46:49 +0300

    docs: add header comment to 04_constraints.sql

---
+++ database/04_constraints.sql
@@ -0,0 +1,8 @@
+-- Author:  Jairus - Database Architect
+-- Purpose: Layer on additional constraints and indexes
+--          once the schema (02) and sample data (03) are
+--          in place. Core NOT NULL / UNIQUE / CHECK /
+--          FK constraints already live in 02_create_tables.sql;
+--          these are the extra business rules and performance
+--          helpers that build on top of that base schema.
+
