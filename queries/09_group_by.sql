3ce8ed7e5f332f980085c5cb2c8aa9a578dca907
Ccode-400 <stanleymngige@gmail.com>
2026-07-10 11:40:56 +0300

    Group artists by genre

--- queries/09_group_by.sql
+++ queries/09_group_by.sql
@@ -5,3 +5,5 @@ SELECT AVG(price) AS average_ticket_price FROM tickets;
 SELECT MIN(price) AS min_price, MAX(price) AS max_price FROM tickets;
 
 SELECT city, COUNT(*) AS attendee_count FROM attendees GROUP BY city ORDER BY attendee_count DESC;
+
+SELECT genre, COUNT(*) AS artist_count FROM artists GROUP BY genre ORDER BY artist_count DESC;
