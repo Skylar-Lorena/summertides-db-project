f20123addc77c83714566d514db2d70b1fda36e7
Ccode-400 <stanleymngige@gmail.com>
2026-07-10 11:38:05 +0300

    Find lowest and highest ticket prices

--- queries/09_group_by.sql
+++ queries/09_group_by.sql
@@ -1,3 +1,5 @@
 SELECT COUNT(*) AS total_attendees FROM attendees;
 
 SELECT AVG(price) AS average_ticket_price FROM tickets;
+
+SELECT MIN(price) AS min_price, MAX(price) AS max_price FROM tickets;
