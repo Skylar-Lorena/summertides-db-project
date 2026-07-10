32dc5f73e7fc8641138b3ff360e6d4c8109813cc
Neville Lascelles <waunaneville@gmail.com>
2026-07-10 12:20:30 +0300

    Add query to show artists with their genres and the stages they perform on

--- queries/10_joins.sql
+++ queries/10_joins.sql
@@ -12,3 +12,14 @@ INNER JOIN tickets AS t
     ON a.attendee_id = t.attendee_id
 ORDER BY a.last_name, a.first_name;
 
+-- 2. Show artists and the stages where they perform.
+SELECT
+    ar.artist_name,
+    ar.genre,
+    st.stage_name
+FROM performances AS p
+INNER JOIN artists AS ar
+    ON p.artist_id = ar.artist_id
+INNER JOIN stages AS st
+    ON p.stage_id = st.stage_id
+ORDER BY ar.artist_name, st.stage_name;
