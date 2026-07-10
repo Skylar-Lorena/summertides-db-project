65794ac739a88d16abc3854e1cd16fb68ae70c89
Ccode-400 <stanleymngige@gmail.com>
2026-07-10 12:08:29 +0300

    Show sponsors linked to the stages

--- queries/10_joins.sql
+++ queries/10_joins.sql
@@ -16,3 +16,8 @@ SELECT v.vendor_name, a.full_name
 FROM vendors v
 JOIN sales sa ON v.vendor_id = sa.vendor_id
 JOIN attendees a ON sa.attendee_id = a.attendee_id;
+
+SELECT s.stage_name, sp.sponsor_name
+FROM stages s
+JOIN stage_sponsors ss ON s.stage_id = ss.stage_id
+JOIN sponsors sp ON ss.sponsor_id = sp.sponsor_id;
