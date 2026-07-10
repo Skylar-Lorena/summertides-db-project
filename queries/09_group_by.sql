06e65b8e02d57538e3b6080f9e4938b39b4aae93
Ccode-400 <stanleymngige@gmail.com>
2026-07-10 11:42:49 +0300

    Sum sales by vendor

--- queries/09_group_by.sql
+++ queries/09_group_by.sql
@@ -7,3 +7,5 @@ SELECT MIN(price) AS min_price, MAX(price) AS max_price FROM tickets;
 SELECT city, COUNT(*) AS attendee_count FROM attendees GROUP BY city ORDER BY attendee_count DESC;
 
 SELECT genre, COUNT(*) AS artist_count FROM artists GROUP BY genre ORDER BY artist_count DESC;
+
+SELECT vendor_id, SUM(amount) AS total_sales_amount FROM sales GROUP BY vendor_id ORDER BY total_sales_amount DESC;
