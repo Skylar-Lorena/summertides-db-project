750b01f8fe2fc232e27f4675dc58951df98e0e8e
Yasin <yasinhassan004@gamil.com>
2026-07-10 11:59:09 +0300

    Add LIMIT queries for top and recent records

--- queries/07_order_limit.sql
+++ queries/07_order_limit.sql
@@ -13,3 +13,31 @@ ORDER BY genre ASC;
 SELECT vendor_name, rating
 FROM vendors
 ORDER BY rating DESC;
+
+SELECT ticket_id,
+       attendee_id,
+       ticket_type,
+       price
+FROM tickets
+ORDER BY price DESC
+LIMIT 5;
+
+SELECT first_name,
+       last_name,
+       registration_date
+FROM attendees
+ORDER BY registration_date ASC
+LIMIT 10;
+
+SELECT ticket_id,
+       attendee_id,
+       purchase_date
+FROM tickets
+ORDER BY purchase_date DESC
+LIMIT 10;
+
+SELECT vendor_name,
+       rating
+FROM vendors
+ORDER BY rating DESC
+LIMIT 3;
