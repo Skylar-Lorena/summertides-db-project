527b5e9f7d3188b587f5ab8561bd7b5654943198
Yasin <yasinhassan004@gamil.com>
2026-07-10 11:57:19 +0300

    Add ascending ORDER BY queries

--- queries/07_order_limit.sql
+++ queries/07_order_limit.sql
@@ -1,2 +1,12 @@
 USE summertides;
 
+-- Display attendees alphabetically.
+SELECT first_name, last_name
+FROM attendees
+ORDER BY last_name ASC, first_name ASC;
+
+-- Show artists ordered by genre.
+SELECT artist_name, genre
+FROM artists
+ORDER BY genre ASC;
+
