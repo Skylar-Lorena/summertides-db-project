667677e3f2f25a0e2f287d59b0204e252675133e
kerry opiyo <kerryopiyo0758@gmail.com>
2026-07-10 13:03:45 +0300

    case-sql

--- queries/08_case.sql
+++ queries/08_case.sql
@@ -18,3 +18,22 @@ SELECT full_name, age,
        END AS age_group
 FROM attendees
 ORDER BY age;
+
+-- Exercise 3: Categorize vendor ratings as Excellent, Good, or Needs Improvement.
+SELECT vendor_name, rating,
+       CASE
+           WHEN rating >= 4.5 THEN 'Excellent'
+           WHEN rating >= 3.5 THEN 'Good'
+           ELSE 'Needs Improvement'
+       END AS rating_category
+FROM vendors
+ORDER BY rating DESC;
+
+-- Exercise 4: Label artists as Local or International based on country.
+SELECT artist_name, country,
+       CASE
+           WHEN country = 'Kenya' THEN 'Local'
+           ELSE 'International'
+       END AS artist_origin
+FROM artists
+ORDER BY artist_name;
