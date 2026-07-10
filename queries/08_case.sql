06105ebbd0f9ada007acd6250f0f2757682e1656
Yasin <yasinhassan004@gamil.com>
2026-07-10 12:12:28 +0300

    Add CASE expression to categorize vendor ratings:

--- queries/08_case.sql
+++ queries/08_case.sql
@@ -22,3 +22,12 @@ SELECT
     END AS age_group
 FROM attendees;
 
+SELECT
+    vendor_name,
+    rating,
+    CASE
+        WHEN rating >= 4.5 THEN 'Excellent'
+        WHEN rating >= 3.5 THEN 'Good'
+        ELSE 'Needs Improvement'
+    END AS rating_category
+FROM vendors;
