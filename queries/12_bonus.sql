a605d573c767802549ea492d34ecaacf8e88ecf3
Ccode-400 <stanleymngige@gmail.com>
2026-07-10 12:19:54 +0300

    Measure how often each stage is used for performances

--- queries/12_bonus.sql
+++ queries/12_bonus.sql
@@ -3,3 +3,9 @@ FROM artists ar
 LEFT JOIN performances p ON ar.artist_id = p.artist_id
 GROUP BY ar.artist_name
 ORDER BY performance_count DESC;
+
+SELECT st.stage_name, COUNT(p.performance_id) AS performance_count
+FROM stages st
+LEFT JOIN performances p ON st.stage_id = p.stage_id
+GROUP BY st.stage_name
+ORDER BY performance_count DESC;
