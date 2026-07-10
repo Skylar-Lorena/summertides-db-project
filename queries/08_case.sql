57e2377868be44bb909526322ffe3dd3be366ca2
Yasin <yasinhassan004@gamil.com>
2026-07-10 12:08:10 +0300

    Add CASE expression to categorize ticket prices

--- queries/08_case.sql
+++ queries/08_case.sql
@@ -1,2 +1,12 @@
 USE summertides;
 
+SELECT
+    ticket_id,
+    ticket_type,
+    price,
+    CASE
+        WHEN price < 4000 THEN 'Budget'
+        WHEN price < 9000 THEN 'Standard'
+        ELSE 'VIP'
+    END AS price_category
+FROM tickets;
