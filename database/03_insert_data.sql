8452e7cca6c6a07649e14ea197fc98059c1dc670
wisejoel77-creator <wisejoel77@gmail.com>
2026-07-10 14:40:13 +0300

    Insert data into artists table

--- database/03_insert_data.sql
+++ database/03_insert_data.sql
@@ -10,3 +10,15 @@ INSERT INTO stages (festival_id, stage_name, capacity, stage_type) VALUES
 (1, 'Coral Acoustic Stage', 1200, 'Acoustic'),
 (1, 'Tide Art Pavilion', 700, 'Art');
 
+INSERT INTO artists (artist_name, genre, country, contact_email) VALUES
+('Sauti Sol', 'Afro-pop', 'Kenya', 'bookings@sautisol.example'),
+('Nviiri the Storyteller', 'Afro-soul', 'Uganda', 'bookings@nviiri.example'),
+('Bensoul', 'R&B', 'Kenya', 'management@bensoul.example'),
+('DJ Pierra Makena', 'Afro-house', 'Kenya', 'bookings@pierrafm.example'),
+('Nikita Kering', 'Afro-pop', 'Kenya', 'team@nikitakering.example'),
+('Femi One', 'Hip-hop', 'Kenya', 'management@femione.example'),
+('DJ Neptune', 'Afrobeats', 'Nigeria', 'bookings@djneptune.example'),
+('Watendawili', 'Afro-fusion', 'Kenya', 'hello@watendawili.example');
+
+
+
