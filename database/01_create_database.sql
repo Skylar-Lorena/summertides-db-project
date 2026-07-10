fce41be5f9d6b206738e3a99c9f89f2298159538
Jairus <bg.jairtom098@gmai.com>
2026-07-10 12:06:44 +0300

    docs: add header comment to 01_create_database.sql

---
+++ database/01_create_database.sql
@@ -0,0 +1,14 @@
+-- =====================================================
+-- SummerTides Festival Database
+-- Author:  Jairus - Database Architect
+-- Run this first, before any other script in the project.
+-- =====================================================
+
+-- Create the database that will power the festival.
+CREATE DATABASE summertides;
+
+\c summertides
+
+-- Verify the database was created by listing all databases
+-- on the server and confirming "summertides" is in the list.
+\l
