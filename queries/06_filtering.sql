e214f4bbaa61cee9770b882cae8e11c0068422a0
Yasin <yasinhassan004@gamil.com>
2026-07-10 11:50:16 +0300

    Add basic WHERE filtering queries

--- queries/06_filtering.sql
+++ queries/06_filtering.sql
@@ -1,1 +1,17 @@
 USE summertides;
+
+-- Show attendees older than 25.
+SELECT first_name, last_name, age
+FROM attendees
+WHERE age > 25;
+
+-- Find attendees from Nairobi.
+SELECT first_name, last_name, city
+FROM attendees
+WHERE city = 'Nairobi';
+
+-- Display VIP tickets only.
+SELECT *
+FROM tickets
+WHERE ticket_type = 'VIP';
+
