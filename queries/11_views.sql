d1f4b5d698bd37bef0f984a2f8188d1e4e773cd0
Neville Lascelles <waunaneville@gmail.com>
2026-07-10 12:25:29 +0300

    feat: add query to list VIP attendees with ticket details

--- queries/11_views.sql
+++ queries/11_views.sql
@@ -1,3 +1,17 @@
 -- Create a reusable view for attendees who purchased VIP tickets.
 DROP VIEW IF EXISTS vip_attendees;
 CREATE VIEW vip_attendees AS
+
+SELECT
+    a.attendee_id,
+    a.first_name,
+    a.last_name,
+    a.email,
+    a.city,
+    t.ticket_type,
+    t.price,
+    t.purchase_date
+FROM attendees AS a
+INNER JOIN tickets AS t
+    ON a.attendee_id = t.attendee_id
+WHERE t.ticket_type = 'VIP';
