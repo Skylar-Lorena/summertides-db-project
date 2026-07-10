283d88d4b4c814904083fc97191fb32880bdccc5
Ccode-400 <stanleymngige@gmail.com>
2026-07-10 11:44:46 +0300

    Count performances by festival day

--- queries/09_group_by.sql
+++ queries/09_group_by.sql
@@ -9,3 +9,5 @@ SELECT city, COUNT(*) AS attendee_count FROM attendees GROUP BY city ORDER BY at
 SELECT genre, COUNT(*) AS artist_count FROM artists GROUP BY genre ORDER BY artist_count DESC;
 
 SELECT vendor_id, SUM(amount) AS total_sales_amount FROM sales GROUP BY vendor_id ORDER BY total_sales_amount DESC;
+
+SELECT festival_day, COUNT(*) AS performance_count FROM performances GROUP BY festival_day ORDER BY performance_count DESC;
